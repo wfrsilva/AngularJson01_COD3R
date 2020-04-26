1735ce2f14dc1218f162e322d97f57e3d98736b1
Wendel Silva <wfrsilva@gmail.com>
2020-04-26 03:00:04 -0300

    Aula Cod3rs F-22/39 Completa : Método Criar Produto
    Aula Cod3rs F-22/39 Completa :
    https://www.cod3r.com.br/courses/take/angular-9-essencial/lessons/11728427-metodo-criar-produto
    
    Método Criar Produto
    Feito um teste com um protudo  temporario para comprovar que db.json recebia esse produto

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -22,6 +22,8 @@ import { ProductCreateComponent } from './components/product/product-create/prod
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -44,7 +46,8 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
     MatListModule,
     MatCardModule,
     MatButtonModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    HttpClientModule
     
   ],
   providers: [],
--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -2,6 +2,7 @@ import { RouterModule } from '@angular/router';
 import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Product } from '../product.model';
 
 
 @Component({
@@ -11,6 +12,11 @@ import { Router } from '@angular/router';
 })
 export class ProductCreateComponent implements OnInit {
 
+  product : Product = {
+    name: '',
+    price: null
+  }
+
   constructor(private productService: ProductService, private router : Router) { }
 
   ngOnInit(): void {
@@ -18,7 +24,11 @@ export class ProductCreateComponent implements OnInit {
   }
 
   createProduct() : void {
-    this.productService.showMessage('Produto criado!')
+    this.productService.create(this.product).subscribe(() =>{
+      this.productService.showMessage('Produto criado!')
+      this.router.navigate(['/products'])
+    })
+    
   }
 
   cancel(): void{
