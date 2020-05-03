f11bbfd15e16b4f0b3d39a27c7cbc67fa1696632
Wendel Silva <wfrsilva@gmail.com>
2020-05-03 00:47:24 -0300

    Atualizacoes de servidores Hosts
    Atualizacoes de servidores Hosts

--- frontend/server.js
+++ frontend/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+//const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 4200;
 
 app.use(express.static(__dirname + '/dist/frontend'));
 
